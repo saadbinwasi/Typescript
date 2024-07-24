269b991aa2082d29dde792d81d95b53cfc98f78b
saad <saad.bin@saad.com>
2024-07-25 02:00:57 +0500

    any in typescript

--- dist/test.js
+++ dist/test.js
@@ -1,7 +1,19 @@
 "use strict";
-console.log("Hello,Worldd");
+// console.log("Hello,Worldd")
 //functions 
-function addtwonumbers(a, b) {
+// function addtwonumbers( a:number,b:number): number {
+//     return a + b
+// }
+// addtwonumbers(2,0)
+//any type
+let age;
+age = 30;
+age = false;
+//array any type
+let things = ['hello', 222, null];
+things.push({ id: 123 });
+//functions 
+function hello(a, b) {
     return a + b;
 }
-addtwonumbers(2, 2);
+hello(3, 200.00);
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,31 @@
-console.log("Hello,Worldd")
+// console.log("Hello,Worldd")
 
 
 //functions 
 
-function addtwonumbers(a:number,b:number):number {
+// function addtwonumbers( a:number,b:number): number {
+//     return a + b
+// }
+
+// addtwonumbers(2,0)
+
+//any type
+
+let age;
+
+age = 30
+age = false
+
+//array any type
+
+let things: any[] = ['hello',222,null]
+
+things.push({id:123})
+
+//functions 
+
+function hello(a:any,b:any): any {
     return a + b
 }
 
-addtwonumbers(2,2)
+hello(3,200.00)
